7d9d9e92177e08034999c38653bc995fd2f00ffc
Abdullah <21100270@lums.edu.pk>
2021-01-15 15:04:38 +0500

    added firebase background notifications service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js");
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage();
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "App";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./firebase-messaging-sw.js")
+    .then(() => {
+      console.log("Background notifications enabled");
+    })
+    .catch((err) => {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
